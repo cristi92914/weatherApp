3bcfe698036499220973119a000eacea3863028d
Balan Tudor-Cristian <balan.tudor.cristian@gmail.com>
2023-09-16 11:50:02 +0300

    adding temperature icon

--- modifyDOM.js
+++ modifyDOM.js
@@ -2,6 +2,7 @@ const temperatureValue = document.getElementById("temperature__value");
 const zoneCity = document.getElementById("zone__city");
 const descriptionP = document.getElementById("description");
 const metric = document.getElementById("temperature__metric");
+const icon = document.getElementById("zone_icon");
 
 export const getZoneP = () => {
   return zoneCity;
@@ -11,8 +12,28 @@ export const setMetric = (units) => {
   metric.textContent = units;
 };
 
+const setIcon = (temp) => {
+  if (temp > 20) {
+    // high
+    // icon.style = ;
+    icon.textContent = "high";
+    icon.src = "./png/high_temperature.png";
+  } else if (temp > 10) {
+    icon.textContent = "medium";
+    icon.src = "./png/medium_temperature.webp";
+    // icon.style.backgroundImage = url("high temperature.png");
+    // medium
+  } else {
+    icon.textContent = "low";
+    icon.src = "./png/low_temperature.webp";
+    // icon.style.backgroundImage = url("high temperature.png");
+    // low
+  }
+};
+
 export const setTemp = (temp) => {
   temperatureValue.textContent = temp;
+  setIcon(temp);
 };
 
 export const setCity = (city) => {
